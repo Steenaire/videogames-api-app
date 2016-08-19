47d19d430e48b41662227a67e03574979d46fb14
Steen Clarke <cmclarkey@gmail.com>
2016-08-18 18:34:38 -0700

    Add seed_dump to backup database entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-Videogame.create(name: "Morrowind", release_date: Date.today, genre: "Fantasy", gameplay: "RPG", steen_rating: 10)
-Videogame.create(name: "Skyrim", release_date: Date.today, genre: "Fantasy", gameplay: "RPG", steen_rating: 8)
-Videogame.create(name: "Team Fortess 2", release_date: Date.today, genre: "Action", gameplay: "FPS", steen_rating: 8)
-Videogame.create(name: "Portal", release_date: Date.today, genre: "SciFi", gameplay: "FP/Puzzle", steen_rating: 10)
-Videogame.create(name: "Mario 3", release_date: Date.today, genre: "Fantasy", gameplay: "Platformer", steen_rating: 7)
-Videogame.create(name: "Dungeon Keeper 2", release_date: Date.today, genre: "Fantasy", gameplay: "RTS", steen_rating: 10)
-Videogame.create(name: "Final Fantasy 7", release_date: Date.today, genre: "Fantasy", gameplay: "RPG", steen_rating: 8)
+Videogame.create!([
+  {name: "Skyrim", release_date: "2016-08-15", genre: "Fantasy", gameplay: "RPG", steen_rating: 8},
+  {name: "Team Fortess 2", release_date: "2016-08-15", genre: "Action", gameplay: "FPS", steen_rating: 8},
+  {name: "Portal", release_date: "2016-08-15", genre: "SciFi", gameplay: "FP/Puzzle", steen_rating: 10},
+  {name: "Mario 3", release_date: "2016-08-15", genre: "Fantasy", gameplay: "Platformer", steen_rating: 7},
+  {name: "Dungeon Keeper 2", release_date: "2016-08-15", genre: "Fantasy", gameplay: "RTS", steen_rating: 10},
+  {name: "Final Fantasy 7", release_date: "2016-08-15", genre: "Fantasy", gameplay: "RPG", steen_rating: 8},
+  {name: "Halo", release_date: "2001-11-15", genre: "SciFi", gameplay: "FPS", steen_rating: 8},
+  {name: "Morrowind", release_date: "2002-05-01", genre: "Fantasy", gameplay: "RPG", steen_rating: 10},
+  {name: "Disks of Tron", release_date: "1983-01-01", genre: "SciFi", gameplay: "Puzzle", steen_rating: 7},
+  {name: "QuackShot", release_date: "1991-01-01", genre: "Action", gameplay: "Side Scroller", steen_rating: 7}
+])

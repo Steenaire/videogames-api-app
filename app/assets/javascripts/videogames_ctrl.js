46efe5d5c1f6116c04a445fc6b566411c819e1c9
Steen Clarke <cmclarkey@gmail.com>
2016-09-04 09:36:45 -0700

    Add way to make post requests from angular videogames controller

--- app/assets/javascripts/videogames_ctrl.js
+++ app/assets/javascripts/videogames_ctrl.js
@@ -3,7 +3,22 @@
     angular.module("app").controller("videogamesCtrl", function($scope, $http) {
         $scope.setup = function() {
             $http.get("/api/v1/videogames.json").then(function(response) {
-                $scope.videogames = response.data
+                $scope.videogames = response.data;
+            });
+        }
+
+        $scope.newVideogame = function(name,releaseDate,gameplay,steenRating,genre) {
+            var videogameData = {
+                name: name,
+                release_date: releaseDate,
+                gameplay: gameplay,
+                steen_rating: steenRating,
+                genre: genre
+            };
+
+
+            $http.post("/api/v1/videogames.json", videogameData).then(function(response) {
+                $scope.videogames.push(videogameData);
             });
         }
     });
